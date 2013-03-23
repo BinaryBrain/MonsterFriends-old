64eb469dfa3f1c4f9b3be5ce319f15a5e8875326
Basile Vu <basile.vu@gmail.com>
2013-03-23 19:59:28 +0100

    Preparing the canvas for the 3 different parts

--- app/static/js/canvas.js
+++ app/static/js/canvas.js
@@ -1,19 +1,21 @@
 var ctx;
-var canWidth, canHeight;
+var cw, ch;
+var headWidth, headHeight = 100;
+var battleWidth, battleHeight;
+var actionWidth, actionHeight = 150;
 var pWidth = 160, pHeight = 160;
 var hpWidth = 400, hpHeight = 50;
-var pBorderSpace = 20;
-var hpBorderSpace = 20;
+var pBordSpace = 20, hpBordSpace = 20;
+var h1, h2, h3;
 var pi = Math.PI;
-
 var p1, p2;
 		
 // load the images and set the basic variables
 function init(){
 	
 	ctx = document.getElementById('canvas').getContext('2d');
-	canWidth = document.getElementById('canvas').width,
-	canHeight = document.getElementById('canvas').height;
+	cw = document.getElementById('canvas').width,
+	ch = document.getElementById('canvas').height;
 	
 	p1 = new Image();
 	p1.onload = function() {
@@ -24,53 +26,56 @@ function init(){
 		p2.src = '../static/img/p2.jpg';
 	}
 	p1.src = '../static/img/p1.jpg';
-		
+	
+	headWidth = cw;
+	battleWidth = cw;
+	actionWidth = cw;
+	
+	battleHeight = ch - (headHeight + actionHeight);
+	
+	h1 = 0;
+	h2 = h1 + headHeight;
+	h3 = h2 + battleHeight;
+	h4 = ch;
 	
 }
 
 // function used to draw the game
 function draw() {
 	
-	// border
-	ctx.lineWidth = 1;
-	ctx.strokeRect(0, 0, canWidth, canHeight);
-	
 	// players 
-	ctx.drawImage(p1, pBorderSpace, canHeight-(pHeight + pBorderSpace));
-	ctx.drawImage(p2, canWidth-(pWidth + pBorderSpace), pBorderSpace);
+	ctx.drawImage(p1, pBordSpace, h3-(pHeight + pBordSpace));
+	ctx.drawImage(p2, battleWidth-(pWidth + pBordSpace), h2 + pBordSpace);
 	
 	// name, lvl, hp
-	/*ctx.strokeRect(hpBorderSpace, hpBorderSpace, hpWidth, hpHeight);
-	ctx.strokeRect(canWidth/2, canHeight-(hpHeight + hpBorderSpace), hpWidth, hpHeight);*/
-	
 	var r = 20;
-	var hbs = hpBorderSpace;
 	var hw = hpWidth;
+	var bw = battleWidth;
 	var hh = hpHeight;
-	var cw = canWidth;
-	var ch = canHeight;
+	var ps = pBordSpace;
+	var hps = hpBordSpace;
 	
 	ctx.lineWidth = 2;
 	ctx.beginPath();
-	ctx.arc(hbs + r, hbs + r, r, pi, 1.5*pi, false);
-	ctx.lineTo(hbs + hw - r, hbs);
-	ctx.arc(hbs + hw - r, hbs + r, r, 1.5*pi, 0, false);
-	ctx.lineTo(hbs + hw, hbs + hh - r);
-	ctx.arc(hbs + hw - r, hbs + hh + r, r, 0, 0.5*pi, false);
-	ctx.lineTo(hbs + r, hbs + hh + 2*r);
-	ctx.arc(hbs + r, hbs + hh + r, r, 0.5*pi, pi, false);
-	ctx.lineTo(hbs, hbs + r);
+	ctx.arc(hps + r, h2 + r, r, pi, 1.5*pi, false);
+	ctx.lineTo(hps + hw - r, h2);
+	ctx.arc(hps + hw - r, h2 + r, r, 1.5*pi, 0, false);
+	ctx.lineTo(hps + hw, h2 + hh - r);
+	ctx.arc(hps + hw - r, h2 + hh + r, r, 0, 0.5*pi, false);
+	ctx.lineTo(hps + r, h2 + hh + 2*r);
+	ctx.arc(hps + r, h2 + hh + r, r, 0.5*pi, pi, false);
+	ctx.lineTo(hps, h2 + r);
 	ctx.stroke();
 	
 	ctx.beginPath();
-	ctx.arc(cw - (hw + hbs - r), ch - (hh + hbs + r), r, pi, 1.5*pi, false);
-	ctx.lineTo(cw - (hbs + r), ch - (hh + hbs + 2*r));
-	ctx.arc(cw - (hbs + r), ch - (hh + hbs + r), r, 1.5*pi, 0, false);
-	ctx.lineTo(cw - (hbs), ch - (hbs + r));
-	ctx.arc(cw - (hbs+r), ch - (hbs + r), r, 0, 0.5*pi, false);
-	ctx.lineTo(cw - (hw + hbs - 2*r), ch - (hbs));
-	ctx.arc(cw - (hw + hbs - r), ch - (hbs+r), r, 0.5*pi, pi, false);
-	ctx.lineTo(cw - (hw + hbs), ch - (hh + hbs + r));
+	ctx.arc(bw - (hw + hps - r), h3 - (hh + hps + r), r, pi, 1.5*pi, false);
+	ctx.lineTo(bw - (hps + r), h3 - (hh + hps + 2*r));
+	ctx.arc(bw - (hps + r), h3 - (hh + hps + r), r, 1.5*pi, 0, false);
+	ctx.lineTo(bw - (hps), h3 - (hps + r));
+	ctx.arc(bw - (hps+r), h3 - (hps + r), r, 0, 0.5*pi, false);
+	ctx.lineTo(bw - (hw + hps - 2*r), h3 - (hps));
+	ctx.arc(bw - (hw + hps - r), h3 - (hps+r), r, 0.5*pi, pi, false);
+	ctx.lineTo(bw - (hw + hps), h3 - (hh + hps + r));
 	ctx.stroke();
 	
 	
