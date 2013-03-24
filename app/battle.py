4f748bb3849c4b1da5b3cb7504d585845d437956
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2013-03-24 14:31:20 +0100

    OK, MONSTER SHOULD BE FIX MOTHERFUCKER.

--- app/battle.py
+++ app/battle.py
@@ -266,7 +266,7 @@ class BattleNamespace(BaseNamespace):
 
         with app.app_context():
             user = User.query.filter_by(fb_id=self.fbid).first()
-            monsters = user.monsters or []
+            monsters = user.monsters.get_stats() or []
         if user is None:
             self.emit("error", "You're not in our database. This is a problem. Please contact us.")
             return False
