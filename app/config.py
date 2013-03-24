c0f772fdc59a34e0f64bcfbb90f6a117d78c3b49
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2013-03-24 16:41:05 +0100

    new config

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 *-*
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://test:test@localhost:5432/monsterfriends'
+SQLALCHEMY_DATABASE_URI = 'postgresql://monsterfriends:monstermotdepasse@localhost:5432/monsterfriends'
 SQLALCHEMY_ECHO = True
 DEBUG = os.getenv('MONSTERFRIENDS_DEBUG', True)
+
