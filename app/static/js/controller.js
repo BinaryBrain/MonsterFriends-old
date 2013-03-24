9ddb4a56cec45da3b931eefbd28fc4889f840259
Kewin Dousse <kewin.d@websud.ch>
2013-03-24 12:34:15 +0100

    Handles the "not in a fight" error

--- app/static/js/controller.js
+++ app/static/js/controller.js
@@ -29,6 +29,9 @@ Controller = {
 		Console.archieveMessage("Error: "+err);
 		console.log("Error: "+err);
 		Console.show();
+		if (err=="Error: You're not in a fight, I can't send you the info...") {
+			Controller.changeScene(Scene.ENEMYCHOICE)
+		}
 	},
 
 	init: function () {
